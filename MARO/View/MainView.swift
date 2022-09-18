b06b9e80b255790cc361e287b194b632213f6d01
phainestha1 <250wons@gmail.com>
2022-09-19 03:11:43 +0900

    [feat] Set sample main view format

--- MARO/View/MainView.swift
+++ MARO/View/MainView.swift
@@ -7,14 +7,30 @@
 
 import SwiftUI
 
+// Model Sample
+struct PromiseModel: Hashable {
+    let number: Int
+    let promise: String
+}
+
 struct MainView: View {
-    let sampleBool: Bool = false
+    // CoreData Sample
+    let sampleArray: [PromiseModel] = [
+        PromiseModel(number: 1, promise: "긍정적인 생각하기"),
+        PromiseModel(number: 2, promise: "긍정적인 생각하기"),
+        PromiseModel(number: 3, promise: "긍정적인 생각하기"),
+        PromiseModel(number: 4, promise: "긍정적인 생각하기"),
+        PromiseModel(number: 5, promise: "긍정적인 생각하기"),
+        PromiseModel(number: 6, promise: "긍정적인 생각하기"),
+        PromiseModel(number: 7, promise: "긍정적인 생각하기"),
+        PromiseModel(number: 8, promise: "긍정적인 생각하기"),
+    ]
     
     var body: some View {
-        if sampleBool {
-            Text("True")
+        if sampleArray.count == 0 {
+            PromiseEmptyView()
         } else {
-            Text("False")
+            PromiseListView(sampleArray)
         }
         
     }// body
@@ -25,3 +41,5 @@ struct MainView_Previews: PreviewProvider {
         MainView()
     }
 }
+
+
