5d9e03262359d63d3783fd0e3ce248c356b8271f
phainestha1 <250wons@gmail.com>
2022-09-20 18:45:37 +0900

    [add] Color asset and extension setting

---
+++ MARO/Extension/Color.swift
@@ -0,0 +1,22 @@
+//
+//  Color.swift
+//  MARO
+//
+//  Created by Noah's Ark on 2022/09/20.
+//
+
+import SwiftUI
+
+extension Color {
+    static let mainPurple: Color = Color("mainPurple")
+    static let mainBlue: Color = Color("mainBlue")
+    static let mainGray: Color = Color("mainGray")
+    static let mainText: Color = Color("mainText")
+    
+    static let subPurple: Color = Color("subPurple")
+    static let subBlue: Color = Color("subBlue")
+    static let subGray: Color = Color("subGray")
+
+    static let backgroundWhite: Color = Color("backgroundWhite")
+    static let backgroundGray: Color = Color("backgroundGray")
+}
